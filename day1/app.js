b6f0c0059ce688b9caa7110cbe4161d9ce04255d
Madhan7671 <madhanmaddasani7671@gmail.com>
2025-06-16 18:34:55 +0530

    first commit

---
+++ day1/app.js
@@ -0,0 +1,41 @@
+window.console.log("hello this is js!");
+//js is dynamically and loosly language
+
+// let age=20;
+// const year=2014;
+// var block=22;
+// console.log(age,year,block)
+// ------------------------------------------
+
+// console.log(age);
+// var age=21; //variable decleration  & initialization
+// console.log(age);
+
+// var age=123; //re-decleration
+// console.log(age);
+
+// -------------------------------------------------
+// temporal dead zone----------
+
+// console.log(age);
+// let age =22;
+// console.log(age);
+
+// -------------------------------------------------
+
+console.log(1+1);
+console.log('1'+1);
+console.log(1+'1');
+console.log('1'+'1');
+console.log(1-1);
+console.log('a'-1);
+console.log(1-'a');
+console.log('1'-'a');
+console.log(1*1);
+console.log('a'*1);
+console.log(1*'a');
+console.log('a'*'1');
+console.log(1/1);
+console.log('a'/1);
+console.log(1/'a');
+console.log('1'/'a');
